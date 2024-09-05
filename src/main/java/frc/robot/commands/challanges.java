0111794069bf8a2fa0910a11fc7100169a71abc4
willmell <whickey27@concordcarlisle.org>
2024-09-05 15:42:17 -0400

    stuff

---
+++ src/main/java/frc/robot/commands/challanges.java
@@ -0,0 +1,14 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.motors.FourMotors;
+import frc.robot.subsystems.motors.FourMotors;
+
+public class challanges extends Command {
+    FourMotors motors;
+
+public void twentysec(){
+    
+
+}
+}
